56b6542d757826e01a324f649d21549c64a1fe15
arnabmindpath <125983511+arnabmindpath@users.noreply.github.com>
2023-03-13 18:03:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let  peoplecount = 0
+
+// This is one way of writing function 
+
+/*function btnclicked() {
+    peoplecount = peoplecount + 1
+    document.getElementById("count-people").innerText = peoplecount;
+    console.log(peoplecount)
+}*/
+
+//  This is another way of writing function
+
+function btnclicked(){
+    let noofpeople = document.getElementById("count-people")
+    console.log(noofpeople)
+    peoplecount = peoplecount + 1
+    console.log(peoplecount)
+    noofpeople.innerText = peoplecount
+}
+
+/*let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function totalracingtime(){
+    let totaltime=0
+    totaltime = lap1 + lap2 + lap3
+    console.log("Total racing time is " + totaltime)
+}
+
+totalracingtime()*/
+
+// Youtube - https://youtu.be/jS4aFq5-91M?t=2405
+
+
